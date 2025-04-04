5a89bb103c12209d1fe028ad88aa36d698da3091
maomao <2081498716@qq.com>
2025-04-04 11:24:09 +0800

    品牌管理静态页面搭建

--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -1,7 +1,31 @@
 <template>
-  <div>品牌管理</div>
+  <el-card>
+    <el-button type="primary">添加品牌</el-button>
+    <el-table border class="table-container">
+      <el-table-column label="序号" width="100" align="center" />
+      <el-table-column label="品牌名称" />
+      <el-table-column label="品牌logo" />
+      <el-table-column label="品牌操作" />
+    </el-table>
+    <el-pagination
+      v-model:current-page="currentPage"
+      v-model:page-size="pageSize"
+      :page-sizes="[3, 5, 7, 9]"
+      :background="true"
+      layout=" prev, pager, next, jumper, ->,sizes, total,"
+      :total="total"
+    />
+  </el-card>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const currentPage = ref(1);
+const pageSize = ref(3);
+const total = ref(400);
+</script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.table-container {
+  margin: 20px 0;
+}
+</style>
