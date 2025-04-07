f219953ca4d810a90d03d605067755ed4310683a
maomao <2081498716@qq.com>
2025-04-07 22:42:22 +0800

    spu添加修改场景页面搭建

---
+++ src/views/product/spu/components/SpuForm.vue
@@ -0,0 +1,53 @@
+<template>
+  <el-form label-width="100px">
+    <el-form-item label="SPU名称">
+      <el-input placeholder="请输入SPU名称" />
+    </el-form-item>
+    <el-form-item label="品牌">
+      <el-select placeholder="请选择品牌">
+        <el-option label="品牌1" value="1" />
+        <el-option label="品牌2" value="2" />
+        <el-option label="品牌3" value="3" />
+      </el-select>
+    </el-form-item>
+    <el-form-item label="SPU描述">
+      <el-input type="textarea" placeholder="请输入SPU描述" />
+    </el-form-item>
+    <el-form-item label="SPU图片">
+      <el-upload list-type="picture-card">
+        <SvgIcon name="plus" />
+      </el-upload>
+    </el-form-item>
+    <el-form-item label="销售属性">
+      <el-select placeholder="请选择销售属性" style="width: 200px">
+        <el-option label="颜色" value="1" />
+        <el-option label="版本" value="2" />
+        <el-option label="尺码" value="3" />
+      </el-select>
+      <el-button style="margin-left: 10px" type="primary" size="default"
+        >添加</el-button
+      >
+    </el-form-item>
+    <el-form-item>
+      <el-table border>
+        <el-table-column label="序号" type="index" width="80" />
+        <el-table-column label="销售属性名称" />
+        <el-table-column label="销售属性值" />
+        <el-table-column label="操作" />
+      </el-table>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" size="default">保存</el-button>
+      <el-button type="primary" size="default" @click="cancel">取消</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(["cancel"]);
+const cancel = () => {
+  emit("cancel", 0);
+};
+</script>
+
+<style scoped></style>
--- src/views/product/spu/index.vue
+++ src/views/product/spu/index.vue
@@ -1,30 +1,42 @@
 <template>
   <el-card>
-    <el-button type="primary" size="default">添加SPU</el-button>
-    <el-table style="margin-top: 10px" border :data="spuData">
-      <el-table-column label="序号" type="index" width="80" />
-      <el-table-column label="SPU名称" prop="spuName" />
-      <el-table-column label="SPU描述" prop="description" />
-      <el-table-column label="操作">
-        <template #="{ row }">
-          <el-button type="primary" size="default">添加</el-button>
-          <el-button type="success" size="default">查看</el-button>
-          <el-button type="warning" size="default">修改</el-button>
-          <el-button type="danger" size="default">删除</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-    <el-pagination
-      style="margin-top: 10px"
-      v-model:current-page="currentPage"
-      v-model:page-size="pageSize"
-      :page-sizes="[3, 5, 7, 9]"
-      :background="true"
-      layout="prev, pager, next, jumper,->,total, sizes"
-      :total="total"
-      @current-change="hasSpu"
-      @size-change="hasSpu(1)"
-    />
+    <div v-show="changeScene === 0">
+      <el-button type="primary" size="default" @click="addSpu"
+        >添加SPU</el-button
+      >
+      <el-table style="margin-top: 10px" border :data="spuData">
+        <el-table-column label="序号" type="index" width="80" />
+        <el-table-column label="SPU名称" prop="spuName" />
+        <el-table-column label="SPU描述" prop="description" />
+        <el-table-column label="操作">
+          <template #="{ row }">
+            <el-button type="primary" size="default">添加</el-button>
+            <el-button type="success" size="default">查看</el-button>
+            <el-button type="warning" size="default" @click="editSpu(row)"
+              >修改</el-button
+            >
+            <el-button type="danger" size="default">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination
+        style="margin-top: 10px"
+        v-model:current-page="currentPage"
+        v-model:page-size="pageSize"
+        :page-sizes="[3, 5, 7, 9]"
+        :background="true"
+        layout="prev, pager, next, jumper,->,total, sizes"
+        :total="total"
+        @current-change="hasSpu"
+        @size-change="hasSpu(1)"
+      />
+    </div>
+    <div v-show="changeScene === 1">
+      <SpuForm @cancel="changeScene = 0" />
+    </div>
+    <div v-show="changeScene === 2">
+      <SkuForm @cancel="changeScene = 0" />
+    </div>
   </el-card>
 </template>
 
@@ -32,10 +44,13 @@
 import { reqHasSpu } from "@/api/product/spu";
 import { ref } from "vue";
 import type { SpuData } from "@/api/product/spu/types";
+import SpuForm from "./components/SpuForm.vue";
+import SkuForm from "./components/SkuForm.vue";
 const currentPage = ref(1);
 const pageSize = ref(3);
 const spuData = ref<SpuData[]>([]);
 const total = ref(0);
+const changeScene = ref(0);
 
 const hasSpu = async (pager = 1) => {
   currentPage.value = pager;
@@ -49,6 +64,12 @@ const hasSpu = async (pager = 1) => {
 onMounted(() => {
   hasSpu();
 });
+const addSpu = () => {
+  changeScene.value = 1;
+};
+const editSpu = (row: SpuData) => {
+  changeScene.value = 1;
+};
 </script>
 
 <style scoped></style>
